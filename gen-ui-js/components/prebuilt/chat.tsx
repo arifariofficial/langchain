d8b9713b89575d1c6c593d441b55b307f97ad564
Ariful Islam <ariful6@yahoo.com>
2024-07-07 05:05:49 +0300

    finish

---
+++ gen-ui-js/components/prebuilt/chat.tsx
@@ -0,0 +1,140 @@
+"use client";
+
+import { useState } from "react";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { useActions } from "@/utils/client";
+import { HumanMessageText } from "./message";
+import { EndpointsContext } from "@/app/agent";
+import { LocalContext } from "@/app/shared";
+
+export interface ChatProps {}
+
+function convertFileToBase64(file: File): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = () => {
+      const base64String = reader.result as string;
+      resolve(base64String.split(",")[1]); // Remove the data URL prefix
+    };
+    reader.onerror = (error) => {
+      reject(error);
+    };
+    reader.readAsDataURL(file);
+  });
+}
+
+function FileUploadMessage({ file }: { file: File }) {
+  return (
+    <div className="flex w-full max-w-fit ml-auto">
+      <p>File uploaded: {file.name}</p>
+    </div>
+  );
+}
+
+export default function Chat() {
+  const actions = useActions<typeof EndpointsContext>();
+
+  const [elements, setElements] = useState<JSX.Element[]>([]);
+  const [history, setHistory] = useState<[role: string, content: string][]>([]);
+  const [input, setInput] = useState("");
+  const [selectedFile, setSelectedFile] = useState<File>();
+
+  async function onSubmit(input: string) {
+    const newElements = [...elements];
+    let base64File: string | undefined = undefined;
+    let fileExtension = selectedFile?.type.split("/")[1];
+    if (selectedFile) {
+      base64File = await convertFileToBase64(selectedFile);
+    }
+
+    const element = await actions.agent({
+      input,
+      chat_history: history,
+      file:
+        base64File && fileExtension
+          ? {
+              base64: base64File,
+              extension: fileExtension,
+            }
+          : undefined,
+    });
+
+    newElements.push(
+      <div className="flex flex-col w-full gap-1 mt-auto" key={history.length}>
+        {selectedFile && <FileUploadMessage file={selectedFile} />}
+        <HumanMessageText content={input} />
+        <div className="flex flex-col gap-1 w-full max-w-fit mr-auto">{element.ui}</div>
+      </div>
+    );
+
+    // consume the value stream to obtain the final value
+    // after which we can append to our chat history state
+    (async () => {
+      let lastEvent = await element.lastEvent;
+      if (typeof lastEvent === "object") {
+        if (lastEvent["invokeModel"]["result"]) {
+          setHistory((prev) => [
+            ...prev,
+            ["user", input],
+            ["assistant", lastEvent["invokeModel"]["result"]],
+          ]);
+        } else if (lastEvent["invokeTools"]) {
+          setHistory((prev) => [
+            ...prev,
+            ["user", input],
+            [
+              "assistant",
+              `Tool result: ${JSON.stringify(
+                lastEvent["invokeTools"]["toolResult"],
+                null
+              )}`,
+            ],
+          ]);
+        } else {
+          console.log("ELSE!", lastEvent);
+        }
+      }
+    })();
+
+    setElements(newElements);
+    setInput("");
+    setSelectedFile(undefined);
+  }
+
+  return (
+    <div className="w-[70vw] overflow-y-scroll h-[80vh] flex flex-col gap-4 mx-auto border-[1px] border-gray-200 rounded-lg p-3 shadow-sm bg-gray-50/25">
+      <LocalContext.Provider value={onSubmit}>
+        <div className="flex flex-col w-full gap-1 mt-auto">{elements}</div>
+      </LocalContext.Provider>
+      <form
+        onSubmit={async (e) => {
+          e.stopPropagation();
+          e.preventDefault();
+          await onSubmit(input);
+        }}
+        className="w-full flex flex-row gap-2"
+      >
+        <Input
+          placeholder="What's the weather like in San Francisco?"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <div className="w-[300px]">
+          <Input
+            placeholder="Upload"
+            id="image"
+            type="file"
+            accept="image/*"
+            onChange={(e) => {
+              if (e.target.files && e.target.files.length > 0) {
+                setSelectedFile(e.target.files[0]);
+              }
+            }}
+          />
+        </div>
+        <Button type="submit">Submit</Button>
+      </form>
+    </div>
+  );
+}
