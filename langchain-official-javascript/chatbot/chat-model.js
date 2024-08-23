9c217cc0faf10f1bf4bd27a2012c2e71602b87f6
Ariful Islam <ariful6@yahoo.com>
2024-08-24 01:40:47 +0300

    chatbot

---
+++ langchain-official-javascript/chatbot/chat-model.js
@@ -0,0 +1,20 @@
+import { ChatOpenAI } from "@langchain/openai";
+import { HumanMessage } from "@langchain/core/messages";
+import { config } from "dotenv";
+import { AIMessage } from "@langchain/core/messages";
+
+config();
+
+const model = new ChatOpenAI({
+	openAIApiKey: process.env.OPENAI_API_KEY,
+	model: "gpt-3.5-turbo",
+	temperature: 0,
+});
+
+const result = await model.invoke([
+	new HumanMessage({ content: "Hi! I'm Bob" }),
+	new AIMessage({ content: "Hello Bob! How can I assist you today?" }),
+	new HumanMessage({ content: "What's my name?" }),
+]);
+
+console.log(result);
