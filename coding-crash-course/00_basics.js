ca6b13e37ec0fbe67ea18a0544b4be15d2bfba2b
Ariful Islam <ariful6@yahoo.com>
2024-08-23 21:39:41 +0300

    javascript

--- coding-crash-course/00_basics.js
+++ coding-crash-course/00_basics.js
@@ -1,8 +1,30 @@
 import { config } from "dotenv";
 config();
 
-import { Configuration, OpenAIApi } from "openai";
+import { OpenAI } from "openai";
 
-const Configuration = new Configuration({
-	apiKey: process.env.OPENAI_API_KEY,
-});
+const openai = new OpenAI();
+
+async function chat(input) {
+	const message = [{ role: "user", content: input }];
+	const response = await openai.chat.completions.create({
+		model: "gpt-3.5-turbo",
+		messages: message,
+		temperature: 0,
+	});
+
+	return response.choices[0].message;
+}
+
+const question = "What is the capital of France?";
+
+chat(question)
+	.then((response) => console.log(response))
+	.catch((error) => console.log(error));
+
+const prompTemlate = `
+    Be very funny when answering the questions
+    Question: {question}
+`;
+
+const prompt = prompTemlate.replace("{question}", question);
